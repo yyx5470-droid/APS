4526fb1db2f21df68e0195c9ad3e465ee7b19aa2
yyx5470-droid <yyx5470@gmail.com>
2026-07-28 19:18:07 +0300

    Update and rename alu.sv to lab_2/alu.sv

--- lab_2/alu.sv
+++ lab_2/alu.sv
@@ -1,10 +1,10 @@
 module alu(
-input  logic [31:0] a_i,
-input  logic [31:0] b_i,
-input  logic [4:0] alu_op_i,
+  input  logic [31:0] a_i,
+  input  logic [31:0] b_i,
+  input  logic [4:0]  alu_op_i,
 
-output logic        flag_o,
-output logic [31:0] result_o
+  output logic        flag_o,
+  output logic [31:0] result_o
 );
 
 import alu_opcodes_pkg::*;
